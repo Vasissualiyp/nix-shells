c42b3c04c64641cc9032383779566e1451886f9c
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-09-08 17:27:17 -0400

    IPR Camb. Forutils doesn't build...

--- camb/shell.nix
+++ camb/shell.nix
@@ -3,7 +3,7 @@ let
   camb = pkgs.python311Packages.buildPythonPackage rec {
 	pname = "camb";
 	version = "1.5.7";
-	format = "wheel";
+	#format = "wheel";
     src = pkgs.fetchFromGitHub {
       owner = "cmbant";
       repo = "CAMB";
@@ -11,13 +11,44 @@ let
       hash = "sha256-e1sBwaCMLVcXrFdoDdANoRezlUBYnnKjjkuGvDkw/t8=";
     };
 
+	buildInputs = [ forutils ];
+	propagatedBuildInputs = [ packaging pkgs.gfortran forutils ];
+	nativeBuildInputs = [ packaging pkgs.gfortran forutils ];
 	postInstall = ''
 	'';
   };
+
+forutils = pkgs.stdenv.mkDerivation {
+  pname = "forutils";
+  version = "1.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "cmbant";
+    repo = "forutils";
+    rev = "37c073b4768e7e6851c6fcb498ed5b9ad85765d8";
+    hash = "sha256-e1sBwaCMLVcXrFdoDdANoRezlUBYnnKjjkuGvDkw/t8="; # Replace this with the actual hash after fetching
+  };
+
+  buildInputs = [ pkgs.gfortran ];
+
+  buildPhase = ''
+  '';
+
+  installPhase = ''
+    #mkdir -p $out/bin
+    cp -r libforutils.a $out/bin
+    echo "Forutils installed in $out/bin"
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Fortran utility programs required by CAMB";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+};
   packaging = pkgs.python311Packages.buildPythonPackage rec {
 	pname = "packaging";
 	version = "24.1";
-	format = "wheel";
+	format = "pyproject";
     src = pkgs.fetchFromGitHub {
       owner = "pypa";
       repo = "packaging";
@@ -25,20 +56,21 @@ let
       hash = "sha256-5ay2MwEw90yc0K3PvyEaxsChX83aJ60jL1rY6q55B2Y=";
     };
 
+	buildInputs = with pkgs.python311Packages; [ pyproject-api flit-core ];
 	postInstall = ''
 	'';
   };
 
 in
   pkgs.mkShell {
-    buildInputs = [
+    buildInputs = with pkgs.python311Packages; [
      pkgs.python311
 
-     pkgs.python311Packages.numpy
-     pkgs.python311Packages.scipy
-     pkgs.python311Packages.sympy
+     numpy
+     scipy
+     sympy
      #pkgs.python311Packages.packaging
-     packaging
+     #packaging
 
      camb
     ];
